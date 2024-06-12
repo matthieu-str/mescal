0cbfc5bba9787aa8d534cc33f5f8ccb0555a5ab6
matthieu-str <matthieu.souttre@gmail.com>
2024-06-12 17:01:08 -0400

    Updated with new functions

--- mescal/__init__.py
+++ mescal/__init__.py
@@ -5,8 +5,10 @@ __all__ = (
     # Add functions and variables you want exposed in `mescal.` namespace here
     "change_location_mapping_file",
     "load_extract_db",
+    "relink_database",
     "concatenate_databases",
     "create_complementary_database",
+    "create_new_database_with_CPC_categories",
 )
 
 __version__ = "0.0.1"
@@ -14,3 +16,4 @@ __version__ = "0.0.1"
 from .location_selection import *
 from .utils import *
 from .link_to_premise import *
+from .CPC import *
