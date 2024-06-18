618e0226efe47347d006ac9edd909ed810be6880
matthieu-str <matthieu.souttre@gmail.com>
2024-06-18 15:44:08 -0400

    improved docstring and added "total only" IW+ LCIA method

--- mescal/normalization.py
+++ mescal/normalization.py
@@ -2,7 +2,12 @@ import pandas as pd
 import ast
 
 
-def tech_type(tech):
+def tech_type(tech: str) -> str:
+    """
+    Returns the short name of the technology type
+    :param tech: (str) technology type
+    :return: (str) short name of the technology type
+    """
     if tech == 'Construction':
         return 'constr'
     elif tech == 'Operation':
@@ -13,9 +18,16 @@ def tech_type(tech):
         raise ValueError(f"Unknown technology type: {tech}")
 
 
-def lcia_methods_short_names(lcia_method):
+def lcia_methods_short_names(lcia_method: str) -> str:
+    """
+    Returns the short name of the LCIA method
+    :param lcia_method: (str) LCIA method
+    :return: (str) short name of the LCIA method
+    """
     if lcia_method == 'IMPACT World+ Damage 2.0.1':
         return 'endpoint'
+    elif lcia_method == 'IMPACT World+ Damage 2.0.1 - Total only':
+        return 'endpoint_tot'
     elif lcia_method == 'IMPACT World+ Midpoint 2.0.1':
         return 'midpoint'
     elif lcia_method == 'IMPACT World+ Footprint 2.0.1':
@@ -24,7 +36,22 @@ def lcia_methods_short_names(lcia_method):
         raise ValueError(f"Unknown LCIA method: {lcia_method}")
 
 
-def normalize_lca_metrics(path, R, f_norm, mip_gap, refactor, lcia_method, impact_abbrev, biogenic=False):
+def normalize_lca_metrics(path: str, R: pd.DataFrame, f_norm: float, mip_gap: float, refactor: float, lcia_method: str,
+                          impact_abbrev: pd.DataFrame, biogenic: bool = False) -> None:
+    """
+    Create a .dat file containing the normalized LCA metrics for AMPL and a csv file containing the normalization
+    factors
+    :param path: (str) path to results folder
+    :param R: (pd.DataFrame) dataframe containing the LCA results
+    :param f_norm: (float) maximum allowed order of magnitude between the smallest and the largest value LCA metrics
+    within an AoP
+    :param mip_gap: (float) values outside the f_norm threshold will be set to mip_gap / f_norm
+    :param refactor: (float) value of refactor to apply for construction metrics for better convergence
+    :param lcia_method: (str) LCIA method to be used
+    :param impact_abbrev: (pd.DataFrame) dataframe containing the impact categories abbreviations
+    :param biogenic: (boolean) whether biogenic carbon flows impact assessment method should be included or not
+    :return: None
+    """
     R_constr = R[R['Type'] == 'Construction']
     R_constr['Value'] *= refactor
 
@@ -39,8 +66,13 @@ def normalize_lca_metrics(path, R, f_norm, mip_gap, refactor, lcia_method, impac
 
     R.Impact_category = R.Impact_category.apply(lambda x: ast.literal_eval(x))
     impact_abbrev.Impact_category = impact_abbrev.Impact_category.apply(lambda x: ast.literal_eval(x))
-    impact_abbrev = impact_abbrev[impact_abbrev.apply(lambda x: x.Impact_category[0] == 'IMPACT World+ Damage 2.0.1',
-                                                      axis=1)]
+
+    if lcia_method == 'IMPACT World+ Damage 2.0.1 - Total only':
+        impact_abbrev = impact_abbrev[impact_abbrev.apply(lambda x:
+                                                          x.Impact_category[0] == 'IMPACT World+ Damage 2.0.1', axis=1)]
+        impact_abbrev = impact_abbrev[impact_abbrev.apply(lambda x: 'Total' in x.Impact_category[2], axis=1)]
+    else:
+        impact_abbrev = impact_abbrev[impact_abbrev.apply(lambda x: x.Impact_category[0] == lcia_method, axis=1)]
 
     R = pd.merge(R, impact_abbrev, on='Impact_category')
     R['max_AoP'] = R.groupby('AoP')['Value'].transform('max')
