56ecf3273518767eb063a89db66e2600eeff9a47
matthieu-str <matthieu.souttre@gmail.com>
2024-09-27 20:46:17 -0400

    updated with classes Dataset and Database

--- mescal/__init__.py
+++ mescal/__init__.py
@@ -3,48 +3,41 @@
 __all__ = (
     "__version__",
     # Add functions and variables you want exposed in `mescal.` namespace here
-    "change_location_mapping_file",
-    "load_extract_db",
-    "relink_database",
-    "load_multiple_databases",
-    "create_complementary_database",
-    "create_new_database_with_CPC_categories",
-    "get_technosphere_flows",
-    "get_production_flow",
-    "get_biosphere_flows",
-    "get_code",
+
+    # utils.py
+    "Database",
+    "Dataset",
     "random_code",
-    "database_list_to_dict",
-    "create_esm_database",
-    "compute_impact_scores",
-    "normalize_lca_metrics",
-    "gen_lcia_obj",
-    "create_or_modify_activity_from_esm_results",
-    "write_wurst_database_to_brightway",
-    "create_new_database_with_esm_results",
-    "get_downstream_consumers",
-    "change_mapping_year",
-    "test_mapping_file",
-    "change_ecoinvent_version_mapping",
-    "load_concatenated_ecoinvent_change_report",
-    "update_unit_conversion_file",
-    "merge_databases",
     "ecoinvent_unit_convention",
-    "correct_esm_and_lca_efficiency_differences",
-    "remove_quebec_flow_in_global_heat_market",
+    "change_mapping_year",
+
+    # "change_location_mapping_file",
+    # "create_complementary_database",
+    # "create_new_database_with_CPC_categories",
+    # "create_esm_database",
+    # "compute_impact_scores",
+    # "normalize_lca_metrics",
+    # "gen_lcia_obj",
+    # "create_or_modify_activity_from_esm_results",
+    # "create_new_database_with_esm_results",
+    # "change_ecoinvent_version_mapping",
+    # "load_concatenated_ecoinvent_change_report",
+    # "update_unit_conversion_file",
+    # "correct_esm_and_lca_efficiency_differences",
+    # "remove_quebec_flow_in_global_heat_market",
 )
 
 __version__ = "1.0.3"
 
-from .location_selection import *
 from .utils import *
-from .link_to_premise import *
-from .CPC import *
-from .double_counting import *
-from .impact_assessment import *
-from .normalization import *
-from .generate_lcia_obj_ampl import *
-from .esm_back_to_lca import *
-from .change_ecoinvent import *
-from .adapt_efficiency import *
-from .modify_inventory import *
+# from .location_selection import *
+# from .link_to_premise import *
+# from .CPC import *
+# from .double_counting import *
+# from .impact_assessment import *
+# from .normalization import *
+# from .generate_lcia_obj_ampl import *
+# from .esm_back_to_lca import *
+# from .change_ecoinvent import *
+# from .adapt_efficiency import *
+# from .modify_inventory import *
