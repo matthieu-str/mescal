b1db85112e3a12896861acdcf756c2e8816f7256
matthieu-str <matthieu.souttre@gmail.com>
2024-10-08 11:20:52 -0400

    added functions from modify_inventory.py

--- mescal/__init__.py
+++ mescal/__init__.py
@@ -19,6 +19,13 @@ __all__ = (
     # change_ecoinvent.py
     "change_ecoinvent_version_mapping",
     "update_unit_conversion_file",
+
+    # modify_inventory.py
+    "change_dac_biogenic_carbon_flow",
+    "change_fossil_carbon_flows_of_biofuels",
+    "remove_quebec_flow_in_global_heat_market",
+    "change_direct_carbon_emissions_by_factor",
+    "add_carbon_dioxide_flow",
 )
 
 __version__ = "1.0.3"
@@ -27,3 +34,4 @@ from .database import Database, Dataset
 from .esm import ESM
 from .utils import *
 from .change_ecoinvent import *
+from .modify_inventory import *
