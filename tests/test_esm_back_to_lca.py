d3ca75345b7ba8e7d7918be1a1a18fb512c79fe6
matthieu-str <matthieu.souttre@gmail.com>
2025-07-14 19:03:58 -0400

    added test for capacity factors harmonization + minor corrections in test_create_new_database_with_esm_results

--- tests/test_esm_back_to_lca.py
+++ tests/test_esm_back_to_lca.py
@@ -18,6 +18,7 @@ dummy_db = [
             {
                 "name": "heat pump production",
                 "product": "heat pump",
+                "location": "CH",
                 "unit": "unit",
                 "amount": 1,
                 "type": "production",
@@ -37,8 +38,9 @@ dummy_db = [
         "comment": "dummy comment",
         "exchanges": [
             {
-                "name": "heat pump production",
+                "name": "double-stage heat pump production",
                 "product": "heat pump",
+                "location": "CH",
                 "unit": "unit",
                 "amount": 1,
                 "type": "production",
@@ -60,6 +62,7 @@ dummy_db = [
             {
                 "name": "market for heat production by heat pump",
                 "product": "heat",
+                "location": "CH",
                 "unit": "megajoule",
                 "amount": 1,
                 "type": "production",
@@ -91,6 +94,7 @@ dummy_db = [
             {
                 "name": "heat production by heat pump",
                 "product": "heat",
+                "location": "CH",
                 "unit": "megajoule",
                 "amount": 1,
                 "type": "production",
@@ -101,30 +105,31 @@ dummy_db = [
                 "name": "market for electricity, low voltage",
                 "product": "electricity, low voltage",
                 "unit": "kilowatt hour",
-                "amount": 0.05,
+                "amount": 0.1,
                 "type": "technosphere",
                 "location": "CH",
                 "database": "ecoinvent-3.9.1-cutoff",
-                "code": "44444",
+                "code": "a44444",
             },
             {
                 "name": "refrigerant R134a production",
                 "product": "refrigerant R134a",
-                "unit": "cubic meter",
-                "amount": 3e-2,
+                "unit": "kilogram",
+                "amount": 3e-6,
                 "type": "technosphere",
                 "location": "CH",
                 "database": "ecoinvent-3.9.1-cutoff",
                 "code": "55555",
             },
             {
-                "name": "double-stage heat pump production",
-                "product": "double-stage heat pump",
+                "name": "heat pump production",
+                "product": "heat pump",
+                "location": "CH",
                 "unit": "unit",
-                "amount": 1e-8,
+                "amount": 1e-6,
                 "type": "technosphere",
                 "database": "ecoinvent-3.9.1-cutoff",
-                "code": "11000",
+                "code": "00000",
             }
         ]
     },
@@ -141,6 +146,7 @@ dummy_db = [
             {
                 "name": "heat production by double-stage heat pump",
                 "product": "heat",
+                "location": "CH",
                 "unit": "megajoule",
                 "amount": 1,
                 "type": "production",
@@ -151,17 +157,17 @@ dummy_db = [
                 "name": "market for electricity, low voltage",
                 "product": "electricity, low voltage",
                 "unit": "kilowatt hour",
-                "amount": 0.04,
+                "amount": 0.08,
                 "type": "technosphere",
                 "location": "CH",
                 "database": "ecoinvent-3.9.1-cutoff",
-                "code": "44444",
+                "code": "a44444",
             },
             {
                 "name": "refrigerant R134a production",
                 "product": "refrigerant R134a",
-                "unit": "cubic meter",
-                "amount": 3e-2,
+                "unit": "kilogram",
+                "amount": 3e-6,
                 "type": "technosphere",
                 "location": "CH",
                 "database": "ecoinvent-3.9.1-cutoff",
@@ -170,8 +176,9 @@ dummy_db = [
             {
                 "name": "double-stage heat pump production",
                 "product": "double-stage heat pump",
+                "location": "CH",
                 "unit": "unit",
-                "amount": 1e-8,
+                "amount": 1e-6,
                 "type": "technosphere",
                 "database": "ecoinvent-3.9.1-cutoff",
                 "code": "11000",
@@ -184,18 +191,19 @@ dummy_db = [
         "location": "CH",
         "unit": "kilowatt hour",
         "database": "ecoinvent-3.9.1-cutoff",
-        "code": "44444",
+        "code": "a44444",
         "classifications": [("CPC", "1000: Electricity")],
         "comment": "dummy comment",
         "exchanges": [
             {
                 "name": "market for electricity, low voltage",
                 "product": "electricity, low voltage",
+                "location": "CH",
                 "unit": "kilowatt hour",
                 "amount": 1,
                 "type": "production",
                 "database": "ecoinvent-3.9.1-cutoff",
-                "code": "44444",
+                "code": "a44444",
             }
         ]
     },
@@ -212,6 +220,7 @@ dummy_db = [
             {
                 "name": "refrigerant R134a production",
                 "product": "refrigerant R134a",
+                "location": "CH",
                 "unit": "cubic meter",
                 "amount": 1,
                 "type": "production",
@@ -233,6 +242,7 @@ dummy_db = [
             {
                 "name": "market for heat",
                 "product": "heat",
+                "location": "CH",
                 "unit": "megajoule",
                 "amount": 1,
                 "type": "production",
@@ -284,6 +294,7 @@ dummy_db = [
             {
                 "name": "heat production by direct electric heater",
                 "product": "heat",
+                "location": "CH",
                 "unit": "megajoule",
                 "amount": 1,
                 "type": "production",
@@ -304,6 +315,7 @@ dummy_db = [
             {
                 "name": "heat production, from oil boiler",
                 "product": "heat",
+                "location": "CH",
                 "unit": "megajoule",
                 "amount": 1,
                 "type": "production",
@@ -325,6 +337,7 @@ dummy_db = [
             {
                 "name": "heat production, from natural gas boiler",
                 "product": "heat",
+                "location": "CH",
                 "unit": "megajoule",
                 "amount": 1,
                 "type": "production",
@@ -365,8 +378,8 @@ model = [
 ]
 
 esm_results = [
-    ['HEAT_PUMP', 10],
-    ['HEAT_PUMP_DOUBLE_STAGE', 25],
+    ['HEAT_PUMP', 10, 0.002],
+    ['HEAT_PUMP_DOUBLE_STAGE', 25, 0.005],
 ]
 
 unit_conversion = [
@@ -376,13 +389,31 @@ unit_conversion = [
     ['HEAT_PUMP_DOUBLE_STAGE', 'Construction', 1/10, 'unit', 'kilowatt'],
 ]
 
+lifetime = [
+    ['HEAT_PUMP', 15, 20],
+    ['HEAT_PUMP_DOUBLE_STAGE', 15, 20],
+]
+
+removed_flows_list = [
+    ['HEAT_PUMP', 'heat', 'heat production by heat pump', 'CH', 'esm_db_name', 'xxx', 1e-06, 'unit', 'heat pump',
+     'heat pump production', 'CH', 'ecoinvent-3.9.1-cutoff', '00000'],
+    ['HEAT_PUMP_DOUBLE_STAGE', 'heat', 'HEAT_PUMP_DOUBLE_STAGE, Operation', 'CH', 'esm_db_name', 'yyy', 1e-06, 'unit',
+     'double-stage heat pump', 'double-stage heat pump production', 'CH', 'ecoinvent-3.9.1-cutoff', '11000'],
+]
+
 mapping = pd.DataFrame(data=mapping, columns=['Name', 'Type', 'Product', 'Activity',
                                               'Location', 'Unit', 'Database', 'Current_code'])
-mapping['New_code'] = len(mapping) * [random_code()]
+
+mapping['New_code'] = mapping.apply(lambda x: random_code(), axis=1)
 mapping_esm_flows_to_CPC = pd.DataFrame(mapping_esm_flows_to_CPC, columns=['Flow', 'CPC'])
 model = pd.DataFrame(model, columns=['Name', 'Flow', 'Amount'])
-esm_results = pd.DataFrame(esm_results, columns=['Name', 'Production'])
+esm_results = pd.DataFrame(esm_results, columns=['Name', 'Production', 'Capacity'])
 unit_conversion = pd.DataFrame(unit_conversion, columns=['Name', 'Type', 'Value', 'LCA', 'ESM'])
+lifetime = pd.DataFrame(lifetime, columns=['Name', 'LCA', 'ESM'])
+removed_flows_list = pd.DataFrame(removed_flows_list, columns=[
+    'Name', 'Product', 'Activity', 'Location', 'Database', 'Code', 'Amount', 'Unit', 'Removed flow product',
+    'Removed flow activity', 'Removed flow location', 'Removed flow database', 'Removed flow code'
+])
 
 
 @pytest.mark.tags("workflow")
@@ -407,7 +438,9 @@ def test_create_new_database_with_esm_results():
         esm_results=esm_results,
         return_database=True,
         write_database=False,
-        harmonize_with_esm=False,
+        harmonize_efficiency_with_esm=False,
+        harmonize_capacity_factor_with_esm=False,
+        remove_background_construction_flows=True,
     )
 
     full_db = esm_with_res_db + esm.main_database
@@ -433,17 +466,75 @@ def test_create_new_database_with_esm_results():
 
     # Test that double-counting removal on the construction flows has been done
     hp_act = new_db_dict_name[
-        "market for heat production by heat pump (HEAT_PUMP)", "heat", "CH", 'esm_db_results'
+        "heat production by heat pump", "heat", "CH", 'esm_db_results'
     ]
     hp_act_technosphere_flows = Dataset(hp_act).get_technosphere_flows()
+    i = 0
     for exc in hp_act_technosphere_flows:
         if exc['name'] == 'heat pump production':
+            i += 1
             assert exc['amount'] == 0
+    assert i == 1
 
+    i = 0
     double_stage_hp_act = new_db_dict_name[
         "heat production by double-stage heat pump (HEAT_PUMP_DOUBLE_STAGE)", "heat", "CH", 'esm_db_results'
     ]
     double_stage_hp_act_technosphere_flows = Dataset(double_stage_hp_act).get_technosphere_flows()
     for exc in double_stage_hp_act_technosphere_flows:
         if exc['name'] == 'double-stage heat pump production':
+            i += 1
             assert exc['amount'] == 0
+    assert i == 1
+
+@pytest.mark.tags("workflow")
+def test_correct_esm_and_lca_capacity_factor_differences():
+
+    esm = ESM(
+        mapping=mapping,
+        model=model,
+        unit_conversion=unit_conversion,
+        mapping_esm_flows_to_CPC_cat=mapping_esm_flows_to_CPC,
+        esm_db_name="esm_db",
+        esm_location="CH",
+        locations_ranking=["CH", "RER", "GLO"],
+        accepted_locations=["CH"],
+        main_database=Database(db_as_list=dummy_db),
+        lifetime= lifetime,
+    )
+
+    esm.df_flows_set_to_zero = removed_flows_list
+
+    esm_with_res_db = esm.create_new_database_with_esm_results(
+        esm_results=esm_results,
+        return_database=True,
+        write_database=False,
+        harmonize_efficiency_with_esm=False,
+        harmonize_capacity_factor_with_esm=True,
+        remove_background_construction_flows=False,
+    )
+
+    full_db = esm_with_res_db + esm.main_database
+    new_db_dict_name = full_db.db_as_dict_name
+
+    hp_act = new_db_dict_name[
+        "heat production by heat pump", "heat", "CH", 'esm_db_results'
+    ]
+    hp_act_technosphere_flows = Dataset(hp_act).get_technosphere_flows()
+    i = 0
+    for exc in hp_act_technosphere_flows:
+        if exc['name'] == 'heat pump production':
+            i+=1
+            assert exc['amount'] == 0.002 * (1/5) / (15 * 10)  # Adjusted for capacity factor difference
+    assert i == 1
+
+    double_stage_hp_act = new_db_dict_name[
+        "heat production by double-stage heat pump (HEAT_PUMP_DOUBLE_STAGE)", "heat", "CH", 'esm_db_results'
+    ]
+    i = 0
+    double_stage_hp_act_technosphere_flows = Dataset(double_stage_hp_act).get_technosphere_flows()
+    for exc in double_stage_hp_act_technosphere_flows:
+        if exc['name'] == 'double-stage heat pump production':
+            i+=1
+            assert exc['amount'] == 0.005 * (1/10) / (15 * 25)  # Adjusted for capacity factor difference
+    assert i == 1
