bd25173fbddf13c2801d6985556c4909cac33942
matthieu-str <matthieu.souttre@gmail.com>
2025-12-09 17:05:46 -0500

    included script from decommission.py in the test

--- tests/test_double_counting.py
+++ tests/test_double_counting.py
@@ -5,7 +5,7 @@ from mescal.esm import ESM
 
 dummy_db = [
     {
-        "name": "heat pump production",
+        "name": "market for heat pump production",
         "reference product": "heat pump",
         "location": "CH",
         "unit": "unit",
@@ -14,13 +14,193 @@ dummy_db = [
         "classifications": [("CPC", "1234: Construction")],
         "exchanges": [
             {
-                "name": "heat pump production",
+                "name": "market for heat pump production",
                 "product": "heat pump",
                 "unit": "unit",
                 "amount": 1,
                 "type": "production",
                 "database": "ecoinvent-3.9.1-cutoff",
                 "code": "00000",
+            },
+            {
+                "name": "heat pump production (type I)",
+                "product": "heat pump",
+                "unit": "unit",
+                "amount": 0.4,
+                "type": "technosphere",
+                "location": "CH",
+                "database": "ecoinvent-3.9.1-cutoff",
+                "code": "00001",
+            },
+            {
+                "name": "heat pump production (type II)",
+                "product": "heat pump",
+                "unit": "unit",
+                "amount": 0.6,
+                "type": "technosphere",
+                "location": "CH",
+                "database": "ecoinvent-3.9.1-cutoff",
+                "code": "00002",
+            }
+        ]
+    },
+    {
+        "name": "heat pump production (type I)",
+        "reference product": "heat pump",
+        "location": "CH",
+        "unit": "unit",
+        "database": "ecoinvent-3.9.1-cutoff",
+        "code": "00001",
+        "classifications": [("CPC", "1234: Construction")],
+        "exchanges": [
+            {
+                "name": "heat pump production (type I)",
+                "product": "heat pump",
+                "unit": "unit",
+                "amount": 1,
+                "type": "production",
+                "database": "ecoinvent-3.9.1-cutoff",
+                "code": "00001",
+            },
+            {
+                "name": "steel production",
+                "product": "steel",
+                "unit": "kilogram",
+                "amount": 50,
+                "type": "technosphere",
+                "location": "CH",
+                "database": "ecoinvent-3.9.1-cutoff",
+                "code": "00003",
+            },
+            {
+                "name": "steel waste, recycled",
+                "product": "waste steel",
+                "unit": "kilogram",
+                "amount": -50,
+                "type": "technosphere",
+                "location": "CH",
+                "database": "ecoinvent-3.9.1-cutoff",
+                "code": "00004",
+            },
+        ]
+    },
+    {
+        "name": "heat pump production (type II)",
+        "reference product": "heat pump",
+        "location": "CH",
+        "unit": "unit",
+        "database": "ecoinvent-3.9.1-cutoff",
+        "code": "00002",
+        "classifications": [("CPC", "1234: Construction")],
+        "exchanges": [
+            {
+                "name": "heat pump production (type II)",
+                "product": "heat pump",
+                "unit": "unit",
+                "amount": 1,
+                "type": "production",
+                "database": "ecoinvent-3.9.1-cutoff",
+                "code": "00002",
+            },
+            {
+                "name": "copper production",
+                "product": "copper",
+                "unit": "kilogram",
+                "amount": 20,
+                "type": "technosphere",
+                "location": "CH",
+                "database": "ecoinvent-3.9.1-cutoff",
+                "code": "00005",
+            },
+            {
+                "name": "copper waste, recycled",
+                "product": "waste copper",
+                "unit": "kilogram",
+                "amount": -20,
+                "type": "technosphere",
+                "location": "CH",
+                "database": "ecoinvent-3.9.1-cutoff",
+                "code": "00006",
+            },
+        ]
+    },
+    {
+        "name": "steel production",
+        "reference product": "steel",
+        "location": "CH",
+        "unit": "kilogram",
+        "database": "ecoinvent-3.9.1-cutoff",
+        "code": "00003",
+        "classifications": [("CPC", "5678: Steel")],
+        "exchanges": [
+            {
+                "name": "steel production",
+                "product": "steel",
+                "unit": "kilogram",
+                "amount": 1,
+                "type": "production",
+                "database": "ecoinvent-3.9.1-cutoff",
+                "code": "00003",
+            }
+        ]
+    },
+    {
+        "name": "copper production",
+        "reference product": "copper",
+        "location": "CH",
+        "unit": "kilogram",
+        "database": "ecoinvent-3.9.1-cutoff",
+        "code": "00005",
+        "classifications": [("CPC", "6789: Copper")],
+        "exchanges": [
+            {
+                "name": "copper production",
+                "product": "copper",
+                "unit": "kilogram",
+                "amount": 1,
+                "type": "production",
+                "database": "ecoinvent-3.9.1-cutoff",
+                "code": "00005",
+            }
+        ]
+    },
+    {
+        "name": "steel waste, recycled",
+        "reference product": "waste steel",
+        "location": "CH",
+        "unit": "kilogram",
+        "database": "ecoinvent-3.9.1-cutoff",
+        "code": "00004",
+        "classifications": [("CPC", "9876: Waste")],
+        "exchanges": [
+            {
+                "name": "steel waste, recycled",
+                "product": "waste steel",
+                "unit": "kilogram",
+                "amount": 1,
+                "type": "production",
+                "database": "ecoinvent-3.9.1-cutoff",
+                "code": "00004",
+            }
+        ]
+    },
+    {
+        "name": "copper waste, recycled",
+        "reference product": "waste copper",
+        "location": "CH",
+        "unit": "kilogram",
+        "database": "ecoinvent-3.9.1-cutoff",
+        "code": "00006",
+        "classifications": [("CPC", "9876: Waste")],
+        "exchanges": [
+            {
+                "name": "copper waste, recycled",
+                "product": "waste copper",
+                "unit": "kilogram",
+                "amount": 1,
+                "type": "production",
+                "database": "ecoinvent-3.9.1-cutoff",
+                "code": "00006",
             }
         ]
     },
@@ -93,7 +273,7 @@ dummy_db = [
                 "code": "55555",
             },
             {
-                "name": "heat pump production",
+                "name": "market for heat pump production",
                 "product": "heat pump",
                 "unit": "unit",
                 "amount": 1e-6,
@@ -148,7 +328,7 @@ dummy_db = [
 mapping = [
     ['HEAT_PUMP', 'Operation', 'heat', 'market for heat production by heat pump', 'CH', '/megajoule',
      'ecoinvent-3.9.1-cutoff'],
-    ['HEAT_PUMP', 'Construction', 'heat pump', 'heat pump production', 'CH', '/unit', 'ecoinvent-3.9.1-cutoff']
+    ['HEAT_PUMP', 'Construction', 'heat pump', 'market for heat pump production', 'CH', '/unit', 'ecoinvent-3.9.1-cutoff']
 ]
 
 mapping_esm_flows_to_CPC = [
@@ -157,6 +337,7 @@ mapping_esm_flows_to_CPC = [
     ['TRANSPORT', ['5000: Transport']],
     ['CONSTRUCTION', ['1234: Construction']],
     ['REFRIGERANTS', ['9999: Refrigerants']],
+    ['DECOMMISSION', ['9876: Waste']],
 ]
 
 model = [
@@ -165,9 +346,15 @@ model = [
     ['HEAT_PUMP', 'CONSTRUCTION', -1],
 ]
 
+unit_conversion = [
+    ['HEAT_PUMP', 'Operation', 'megajoule', 'kilowatt hour', 3.6],
+    ['HEAT_PUMP', 'Construction', 'unit', 'kilowatt', 1/30],
+]
+
 mapping = pd.DataFrame(data=mapping, columns=['Name', 'Type', 'Product', 'Activity', 'Location', 'Unit', 'Database'])
 mapping_esm_flows_to_CPC = pd.DataFrame(mapping_esm_flows_to_CPC, columns=['Flow', 'CPC'])
 model = pd.DataFrame(model, columns=['Name', 'Flow', 'Amount'])
+unit_conversion = pd.DataFrame(data=unit_conversion, columns=['Name', 'Type', 'LCA', 'ESM', 'Value'])
 
 
 @pytest.mark.tags("workflow")
@@ -179,7 +366,9 @@ def test_create_esm_database():
         mapping_esm_flows_to_CPC_cat=mapping_esm_flows_to_CPC,
         esm_db_name='esm_db_name',
         main_database=Database(db_as_list=dummy_db),
-        unit_conversion=pd.DataFrame(),
+        unit_conversion=unit_conversion,
+        extract_eol_from_construction=True,
+        remove_double_counting_to=['Operation', 'Construction'],
     )
 
     esm_db = esm.create_esm_database(
@@ -206,5 +395,27 @@ def test_create_esm_database():
             assert exc['amount'] == 3e-6  # refrigerant flow has to be kept
         elif exc['name'] == 'HEAT_PUMP, Construction':
             assert exc['amount'] == 0  # construction flow has to be removed
+        else:
+            raise ValueError(f"Unexpected technosphere exchange: {exc}")
+
+    act_constr = esm_db_dict_name["heat pump production (type I)", "heat pump", "CH", "esm_db_name"]
+    act_constr_technosphere = Dataset(act_constr).get_technosphere_flows()
+
+    for exc in act_constr_technosphere:
+        if exc['name'] == 'steel production':
+            assert exc['amount'] == 50  # steel production flow has to be kept
+        elif exc['name'] == 'steel waste, recycled':
+            assert exc['amount'] == 0  # steel waste flow has to be removed (moved to the decommission dataset)
+        else:
+            raise ValueError(f"Unexpected technosphere exchange: {exc}")
+
+    act_decom = esm_db_dict_name["HEAT_PUMP, Decommission", "used heat pump", "CH", "esm_db_name"]
+    act_decom_technosphere = Dataset(act_decom).get_technosphere_flows()
+
+    for exc in act_decom_technosphere:
+        if exc['name'] == 'steel waste, recycled':
+            assert exc['amount'] == -50 * 0.4  # steel waste flow scaled to the functional unit
+        elif exc['name'] == 'copper waste, recycled':
+            assert exc['amount'] == -20 * 0.6  # copper waste flow scaled to the functional unit
         else:
             raise ValueError(f"Unexpected technosphere exchange: {exc}")
